15e30ef3f686e166f00cc9c8b06e734177b93a90
Emanuel Freitas <efreitasn@hotmail.com>
2017-11-26 19:56:26 -0200

    Remove const DB from config/sequelize.js

--- server/config/sequelize.js
+++ server/config/sequelize.js
@@ -1,7 +1,5 @@
 const Sequelize = require('sequelize');
 
-const DB = process.env.DB;
-
 module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
